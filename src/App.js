61ec7d1898ad94426c68119390603fa0e78ab327
Faizul Osman <faizulosmanrabby@gmail.com>
2022-12-23 00:04:07 +0600

    pie chart implement completely done

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
 import "./App.css";
 import ChartJs from "./components/ChartJs";
-import Recharts from "./components/Recharts";
 
 function App() {
   return (
     <div className="App">
-      {/* ========== React Chart JS ========== */}
       <h1 className="text-xl md:text-3xl lg:text-6xl font-bold text-[#1d548d]">
         PIE CHART <br />
         <span className="italic">INFOGRAPHIC</span>
       </h1>
       <div className="mt-10 lg:mt-20 relative">
         <ChartJs className=""></ChartJs>
-        <div className="absolute top-0 sm:top-5 md:top-10 lg:top-14 left-0 sm:left-5 md:left-10 lg:left-14 text-right text-gray-500">
+        <div className="absolute hidden sm:block top-0 sm:top-5 md:top-10 lg:top-14 left-0 sm:left-5 md:left-10 lg:left-14 text-right text-gray-500">
           <h3 className="text-xl font-semibold">01</h3>
           <p className="text-xs sm:text-sm font-semibold">TITLE HERE</p>
         </div>
-        <div className="absolute top-0 sm:top-5 md:top-10 lg:top-14 right-0 sm:right-5 md:right-10 lg:right-14 text-right text-gray-500">
+        <div className="absolute hidden sm:block top-0 sm:top-5 md:top-10 lg:top-14 right-0 sm:right-5 md:right-10 lg:right-14 text-right text-gray-500">
           <h3 className="text-xl font-semibold">02</h3>
           <h4 className="text-xs sm:text-sm font-semibold">TITLE HERE</h4>
         </div>
-        <div className="absolute top-[60%] right-0 sm:right-5 md:right-10 lg:right-14 text-right text-gray-500">
+        <div className="absolute hidden sm:block top-[60%] right-0 sm:right-5 md:right-10 lg:right-14 text-right text-gray-500">
           <h3 className="text-xl font-semibold">03</h3>
           <h4 className="text-xs sm:text-sm font-semibold">TITLE HERE</h4>
         </div>
-        <div className="absolute bottom-0 right-[45%] text-right text-gray-500">
+        <div className="absolute hidden sm:block bottom-0 right-[45%] text-right text-gray-500">
           <h3 className="text-xl font-semibold">04</h3>
           <h4 className="text-xs sm:text-sm font-semibold">TITLE HERE</h4>
         </div>
-        <div className="absolute bottom-10 sm:bottom-32 md:bottom-40 lg:bottom-64 left-0 text-right text-gray-500">
+        <div className="absolute hidden sm:block bottom-10 sm:bottom-32 md:bottom-40 lg:bottom-64 left-0 text-right text-gray-500">
           <h3 className="text-xl font-semibold">05</h3>
           <h4 className="text-xs sm:text-sm font-semibold">TITLE HERE</h4>
         </div>
       </div>
-      <div style={{ stroke: "0px" }}>{/* <Recharts></Recharts> */}</div>
     </div>
   );
 }
--- src/components/ChartJs.js
+++ src/components/ChartJs.js
@@ -29,7 +29,8 @@ const ChartJs = () => {
           "#2badea",
           "#f52384",
         ],
-        borderWidth: 0,
+        borderWidth: [0, 3, 0, 0, 0],
+        borderAlign: "inner",
       },
       {
         data: [3, 3, 3, 3, 3],
@@ -77,36 +78,46 @@ const ChartJs = () => {
     <div className="relative">
       <PolarArea data={data} />
       <div className="absolute top-[27%] sm:top-[19%] md:top-[18%] lg:top-[17%] left-[30%] sm:left-[32%] md:left-[35%] text-right text-gray-500">
-        <h3 className="text-xs md:text-xl font-bold sm:text-[#2badea]">17%</h3>
+        <h3 className="text-xs md:text-xl lg:text-5xl font-bold sm:text-[#2badea]">
+          17%
+        </h3>
       </div>
       <div className="absolute hidden sm:block top-[28%] left-[22%] text-right text-gray-500">
-        <img src={icon1} className="w-5 md:w-7 h-5 md:h-7" alt="" />{" "}
+        <img src={icon1} className="w-5 md:w-7 lg:w-16" alt="" />{" "}
       </div>
       <div className="absolute top-[54%] sm:top-[50%] left-[25%] sm:left-[18%] text-right text-gray-500">
-        <h3 className="text-xs md:text-xl font-bold sm:text-[#fcb702]">15%</h3>
+        <h3 className="text-xs md:text-xl lg:text-5xl font-bold sm:text-[#fcb702]">
+          15%
+        </h3>
       </div>
       <div className="absolute hidden sm:block top-[63%] left-[22%] text-right text-gray-500">
-        <img src={icon5} className="w-5 md:w-7 h-5 md:h-7" alt="" />{" "}
+        <img src={icon5} className="w-5 md:w-7 lg:w-16" alt="" />{" "}
       </div>
       <div className="absolute bottom-[27%] sm:bottom-[22%] left-[46%] sm:left-[40%] text-right text-gray-500">
-        <h3 className="text-xs md:text-md font-bold sm:text-[#98c417]">15%</h3>
+        <h3 className="text-xs md:text-md lg:text-5xl font-bold sm:text-[#98c417]">
+          13%
+        </h3>
       </div>
       <div className="absolute hidden sm:block bottom-[22%] right-[40%] text-right text-gray-500">
-        <img src={icon4} className="w-4 md:w-6 h-4 md:h-6" alt="" />{" "}
+        <img src={icon4} className="w-4 md:w-6 lg:w-16" alt="" />{" "}
       </div>
 
-      <div className="absolute top-[55%] sm:top-[50%] right-[27%] sm:right-[25%] text-right text-gray-500">
-        <h3 className="text-xs md:text-md font-bold sm:text-[#f52384]">10%</h3>
+      <div className="absolute top-[55%] sm:top-[50%] right-[27%] sm:right-[25%] lg:right-[23%] text-right text-gray-500">
+        <h3 className="text-xs md:text-md lg:text-4xl font-bold sm:text-[#f52384]">
+          11%
+        </h3>
       </div>
       <div className="absolute hidden sm:block top-[63%] right-[30%] text-right text-gray-500">
-        <img src={icon3} className="w-4 md:w-5 h-4 md:h-5" alt="" />{" "}
+        <img src={icon3} className="w-4 md:w-5 lg:w-12" alt="" />{" "}
       </div>
 
-      <div className="absolute top-[32%] md:top-[33%] lg:top-[30%] right-[37%] lg:right-[42%] text-right text-gray-500">
-        <h3 className="text-xs md:text-md font-bold md:text-[#1d548d]">5%</h3>
+      <div className="absolute top-[32%] md:top-[33%] lg:top-[29%] right-[37%] lg:right-[40%] text-right text-gray-500">
+        <h3 className="text-xs md:text-md lg:text-4xl font-bold md:text-[#1d548d]">
+          9%
+        </h3>
       </div>
-      <div className="absolute hidden lg:block top-[37%] right-[33%] text-right text-gray-500">
-        <img src={icon2} className="w-3 md:w-4 h-3 md:h-4" alt="" />{" "}
+      <div className="absolute hidden lg:block top-[37%] right-[33%] lg:right-[31%] text-right text-gray-500">
+        <img src={icon2} className="w-3 md:w-4 lg:w-10" alt="" />{" "}
       </div>
     </div>
   );
